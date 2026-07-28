ea22238ea3c1be4c16c53c3b8aa1756c965a4707
Anisha-0505007 <ghoshanisha557@gmail.com>
2026-07-29 00:07:35 +0530

    Time: 1898 ms (5.22%), Space: 62.1 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,31 +1,49 @@
 class Solution {
+
+    public String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
     public String smallestPalindrome(String s) {
+
+        if (s.length() == 0 || s.length() == 1) {
+            return s;
+        }
+
+        String news = "";
+        int n = s.length();
+
+        char[] chars = s.toCharArray();
+
         int[] freq = new int[26];
 
         // Count frequencies
-        for (char c : s.toCharArray()) {
+        for (char c : chars) {
             freq[c - 'a']++;
         }
 
-        StringBuilder left = new StringBuilder();
+        String firstHalf = "";
         String middle = "";
 
-        // Build left half and find middle character
+        // Build first half
         for (int i = 0; i < 26; i++) {
-            if (freq[i] % 2 == 1) {
-                middle = String.valueOf((char) ('a' + i));
+
+            while (freq[i] >= 2) {
+                firstHalf += (char) ('a' + i);
+                freq[i] -= 2;
             }
 
-            for (int j = 0; j < freq[i] / 2; j++) {
-                left.append((char) ('a' + i));
+            if (freq[i] == 1) {
+                middle = String.valueOf((char) ('a' + i));
             }
         }
 
-        StringBuilder ans = new StringBuilder();
-        ans.append(left);
-        ans.append(middle);
-        ans.append(new StringBuilder(left).reverse());
+        if (n % 2 == 0) {
+            news = firstHalf + reverse(firstHalf);
+        } else {
+            news = firstHalf + middle + reverse(firstHalf);
+        }
 
-        return ans.toString();
+        return news;
     }
 }
