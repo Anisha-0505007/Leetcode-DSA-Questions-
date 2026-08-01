f4cd44659d8f25b18a1a020dca2aa1042eb44a12
Anisha-0505007 <ghoshanisha557@gmail.com>
2026-08-01 23:37:39 +0530

    Time: 1 ms (6.19%), Space: 42.8 MB (93.88%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> r = new ArrayList<Integer>();
+        if(root == null){
+            return r;
+        }
+        r.addAll(inorderTraversal(root.left));
+        r.add(root.val);
+        r.addAll(inorderTraversal(root.right));
+
+        return r;
+
+    }
+}
