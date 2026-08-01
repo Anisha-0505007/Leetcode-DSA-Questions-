237cd88392d449b0b3f6057289f7d428d4f24195
Anisha-0505007 <ghoshanisha557@gmail.com>
2026-08-02 01:09:16 +0530

    Time: 2 ms (5.03%), Space: 44.2 MB (89.85%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+
+        if (root == null){
+            return ans;
+        }
+
+        q.offer(root);
+        while(!q.isEmpty()){
+            int LevelNum = q.size();
+            ArrayList<Integer> subl = new ArrayList<>();
+
+            for(int i=0;i<LevelNum;i++){
+                if(q.peek().left!=null){
+                    q.offer(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.offer(q.peek().right);
+                }
+                subl.add(q.poll().val);
+                
+            }
+            ans.add(subl);
+            
+            
+        }
+        List<List<Integer>> ansnew = new LinkedList<List<Integer>>();
+            for(int i= ans.size()-1; i>=0;i--){
+                ansnew.add(ans.get(i));
+            }
+        return ansnew;
+    }
+}
